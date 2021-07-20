8fb9c29eefb31aac226016852edb251cd5849583
冨田悠仁 <tomitayuuhitoshi@tomitayuuhitoshinoMacBook-Air.local>
2021-07-21 00:28:18 +0900

    first commit

---
+++ src/main/java/com/example/am/ProjectAmApplication.java
@@ -0,0 +1,13 @@
+package com.example.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAmApplication.class, args);
+	}
+
+}
